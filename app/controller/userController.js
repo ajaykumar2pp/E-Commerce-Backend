7c3474a06ae0ac8cd6541a3d760f6936668b1f9f
ajaykumar2pp <ajay2kumarpp@gmail.com>
2023-10-01 08:39:36 +0530

    password hash

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -1,39 +1,45 @@
 const User = require("../model/user");
+const bcrypt = require('bcryptjs');
 
 function userController() {
   return {
     async newUser(req, resp) {
       try {
         const { username, email, password } = req.body;
+
+        // Hash the password before storing it in the database
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
         let createUser = await User.create({
           username,
           email,
-          password,
+          password:hashedPassword
         });
         createUser.save();
         createUser = createUser.toObject();
         delete createUser.password;
         resp.status(201).json({ data: { user: createUser } });
+        console.log(createUser)
       } catch (err) {
         resp.status(500).json({ error: "Failed User" });
       }
     },
 
-    async loginUser(req,resp){
+    async loginUser(req, resp) {
       console.log(req.body)
-      if(req.body.password && req.body.email){
+      if (req.body.password && req.body.email) {
         let user = await User.findOne(req.body).select("-password");
-        if(user){
+        if (user) {
           resp.send(user)
         }
-        else{
-          resp.send({result:"Plz Check Passowrd or Email"})
+        else {
+          resp.send({ result: "Plz Check Passowrd or Email" })
         }
-      }else{
-        resp.send({result:"Invalid User"})
+      } else {
+        resp.send({ result: "Invalid User" })
       }
-    
-   
+
+
     },
   };
 }
